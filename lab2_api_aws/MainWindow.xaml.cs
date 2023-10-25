39784bfe4dbea4cc662f36bc32af9602fa3fcbd2
Lea-MM <lmagbal1@my.centennialcollege>
2023-10-24 22:35:05 -0400

    Add project files.

---
+++ lab2_api_aws/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab2_api_aws
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start("cmd", $"/c start {"https://github.com/Lea-MM/AWS-Service1"}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while trying to open the URL: {ex.Message}");
+            }
+        }
+
+        private void btnLogin(object sender, RoutedEventArgs e)
+        {
+            AccountWindow accountWindow = new AccountWindow();
+            accountWindow.Show();
+            this.Close();
+        }
+    }
+}
